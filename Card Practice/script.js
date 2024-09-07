35948d9d562e2b55788464f7bba5b22dd5577d0f
Ashutosh Singh Kushwaha <152424464+OnlyForAshutosh20799@users.noreply.github.com>
2024-09-08 00:30:51 +0530

    Delete Card Practice directory

--- Card Practice/script.js
+++
@@ -1,7 +0,0 @@
-let likeCount = 0;
-const likeIcon = document.querySelector('.fa-regular fa-heart');
-const likeCountElement = document.querySelector('.lolo');
-likeIcon.addEventListener('click', function(){
-    likeCount++;
-    likeCountElement.textContent=likeCount;
-});
